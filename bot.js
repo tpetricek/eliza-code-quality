906962a86db97ca4ed3e5fb65aa8f5d41e7e0214
Oliver <ojm8@kent.ac.uk>
2019-03-18 14:24:52 +0000

    Added function reply and refactored saySomething

--- bot.js
+++ bot.js
@@ -34,7 +34,7 @@ function matchPattern(p, w, pi, wi) {
   if (pi == p.length && wi == w.length) return [];
   if (pi == p.length) {
         return null;
-  } 
+		} 
   if (p[pi] == "*") {
     // Try matching '*' with anything between zero or all remaining words
     for(var l = 0; l <= w.length-wi; l++) {
@@ -46,42 +46,41 @@ function matchPattern(p, w, pi, wi) {
         for(var i = 0; i < sub.length; i++) {
           for(var j = 0; j < replacements.length; j++) {
             if (sub[i] == replacements[j][0]) sub[i] = replacements[j][1];
-          }
-        }
-        return [sub.join(' ')].concat(res);      
-      }
-    }
+			}
+		}
+      return [sub.join(' ')].concat(res);}}
     return null;
-  }
+	}
   if (wi == w.length)
     return null;
-  if (p[pi] == w[wi]) 
-  {
-      return matchPattern(p, w, pi+1, wi+1)
+  if (p[pi] == w[wi]) {
+    return matchPattern(p, w, pi+1, wi+1)
+    }
   }
-}
 
 function saySomething() {
   var message = document.getElementById('message').value;
   var conversation = document.getElementById('conversation');
-  var answer = "Can you tell me more about that?";
-  var words = message.split(' ')
+  //var answer = "Can you tell me more about that?";
   
-  // Iterate over all rules and find the first one that matches
-  for(var i = 0; i<rules.length; i++) 
-  {
-  var match = matchPattern(rules[i].pattern, words, 0, 0)
-  if (match) {
-  // Reconstruct a reply - if a token is a number, find the
-  // matched word from 'match', otherwise just append the word
-  var res = ""
-  for(var token of rules[i].reply) {
-    if (typeof(token) == "number") res += match[token]
-      else res += token;
-  }
-  answer = res;
-  }
-  }
+  
+  answer = reply(message);
   conversation.innerHTML += "<p><strong>You:</strong> " + message + "</p>";
-    conversation.innerHTML += "<p><strong>Eliza:</strong> " + answer + "</p>";
+  conversation.innerHTML += "<p><strong>Eliza:</strong> " + answer + "</p>";}
+  
+function reply(message){
+	var words = message.split(' ')
+  // Iterate over all rules and find the first one that matches
+  for(var i = 0; i<rules.length; i++) {
+    var match = matchPattern(rules[i].pattern, words, 0, 0)
+    if (match) {
+      // Reconstruct a reply - if a token is a number, find the
+      // matched word from 'match', otherwise just append the word
+      var res = ""
+      for(var token of rules[i].reply) {
+        if (typeof(token) == "number") res += match[token]
+        else res += token;}
+      return res;}
+	  }
+  return "Can you tell me more about that?";
 }
