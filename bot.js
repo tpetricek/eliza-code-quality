27be756ed7c6e5b5f04537187853318a8b631f2b
Lucas V <lucasjvicic@gmail.com>
2020-04-04 18:19:03 -0400

    Improved indentation for saySomething() and matchPattern()

--- bot.js
+++ bot.js
@@ -34,9 +34,11 @@ var replacements =
 // matched word(s) that correspond to all '*' in the pattern.
 function matchPattern(p, w, pi, wi) {
   if (pi == p.length && wi == w.length) return [];
+
   if (pi == p.length) {
         return null;
   }
+
   if (p[pi] == "*") {
     // Try matching '*' with anything between zero or all remaining words
     for(var l = 0; l <= w.length-wi; l++) {
@@ -50,15 +52,19 @@ function matchPattern(p, w, pi, wi) {
             if (sub[i] == replacements[j][0]) sub[i] = replacements[j][1];
           }
         }
-        return [sub.join(' ')].concat(res);
+
+      return [sub.join(' ')].concat(res);
       }
     }
+
     return null;
+
   }
+
   if (wi == w.length)
     return null;
-  if (p[pi] == w[wi])
-  {
+
+  if (p[pi] == w[wi]){
       return matchPattern(p, w, pi+1, wi+1)
   }
 }
@@ -70,20 +76,22 @@ function saySomething() {
   var words = message.split(' ')
 
   // Iterate over all rules and find the first one that matches
-  for(var i = 0; i<rules.length; i++)
-  {
-  var match = matchPattern(rules[i].pattern, words, 0, 0)
-  if (match) {
+  for(var i = 0; i<rules.length; i++) {
+    var match = matchPattern(rules[i].pattern, words, 0, 0)
+
+    if (match) {
   // Reconstruct a reply - if a token is a number, find the
   // matched word from 'match', otherwise just append the word
-  var res = ""
-  for(var token of rules[i].reply) {
-    if (typeof(token) == "number") res += match[token]
-      else res += token;
-  }
-  answer = res;
-  }
-  }
+      var res = ""
+        for(var token of rules[i].reply) {
+          if (typeof(token) == "number") res += match[token]
+          else res += token;
+        }
+
+        answer = res;
+
+      }
+    }
   conversation.innerHTML += "<p><strong>You:</strong> " + message + "</p>";
     conversation.innerHTML += "<p><strong>Eliza:</strong> " + answer + "</p>";
 }
