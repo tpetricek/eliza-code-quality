fd4fe524041f31270cb4389453a4acb79f757347
Caleb Pike <calebpike91@gmail.com>
2020-11-25 11:08:34 +0000

    some small code quality improvements

--- bot.js
+++ bot.js
@@ -3,27 +3,37 @@
 // from zero) that refer to matched '*' word lists in the pattern.
 var rules =
 	[
-		{ "pattern": [ "*", "I", "am", "*" ],
-		"reply": [ "Can you explain what made you ", 1, "?" ] },
-		{ "pattern": [ "*", "I", "remember", "*" ],
-		"reply": [ "What made you think about ", 1, "?" ] },
-		{ "pattern": [ "*", "loves", "*" ],
-		"reply": [ "How does that make you feel about ", 0, "?" ] },
-		{ "pattern": [ "*", "mother", "*" ],
-		"reply": [ "Tell me more about your family." ] },
-		{ "pattern": [ "*", "father", "*" ],
-		"reply": [ "Tell me more about your family." ] },
+		{
+			"pattern": ["*", "I", "am", "*"],
+			"reply": ["Can you explain what made you ", 1, "?"]
+		},
+		{
+			"pattern": ["*", "I", "remember", "*"],
+			"reply": ["What made you think about ", 1, "?"]
+		},
+		{
+			"pattern": ["*", "loves", "*"],
+			"reply": ["How does that make you feel about ", 0, "?"]
+		},
+		{
+			"pattern": ["*", "mother", "*"],
+			"reply": ["Tell me more about your family."]
+		},
+		{
+			"pattern": ["*", "father", "*"],
+			"reply": ["Tell me more about your family."]
+		},
 	]
 
 // Some words need to be replaced when recomposing the reply so that
 // "I remember my dog" -> "What made you think about *your* dog?"
 var replacements =
 	[
-		[ "my", "your"],
-		[ "My", "your"],
-		[ "me", "you" ],
-		[ "Me", "you" ],
-		[ "I" , "you" ],
+		["my", "your"],
+		["My", "your"],
+		["me", "you"],
+		["Me", "you"],
+		["I", "you"],
 	]
 
 // Checks whether given words match given pattern (patternIndex and wordIndex are
@@ -39,14 +49,14 @@ function matchPattern(pattern, words, patternIndex, wordIndex) {
 	}
 	if (pattern[patternIndex] == "*") {
 		// Try matching '*' with anything between zero or all remaining words		
-		for(var l = 0; l <= words.length-wordIndex; l++) {
-			var res = matchPattern(pattern, words, patternIndex+1, wordIndex+l);
+		for (var l = 0; l <= words.length - wordIndex; l++) {
+			var res = matchPattern(pattern, words, patternIndex + 1, wordIndex + l);
 			if (res) {
 				// If matching succeeded, apply replacements and add
 				// words matched against the current '*' to returned result
-				var sub = words.slice(wordIndex, wordIndex+l);
-				for(var i = 0; i < sub.length; i++) {
-					for(var j = 0; j < replacements.length; j++) {
+				var sub = words.slice(wordIndex, wordIndex + l);
+				for (var i = 0; i < sub.length; i++) {
+					for (var j = 0; j < replacements.length; j++) {
 						if (sub[i] == replacements[j][0]) {
 							sub[i] = replacements[j][1];
 						}
@@ -61,36 +71,36 @@ function matchPattern(pattern, words, patternIndex, wordIndex) {
 		return null;
 	}
 	if (pattern[patternIndex] == words[wordIndex]) {
-		return matchPattern(pattern, words, patternIndex+1, wordIndex+1);
+		return matchPattern(pattern, words, patternIndex + 1, wordIndex + 1);
 	}
 }
 
 function saySomething() {
 	var message = document.getElementById('message').value;
-	var answer = reply(message);	
-	var conversation = document.getElementById('conversation');	
+	var answer = reply(message);
+	var conversation = document.getElementById('conversation');
 	conversation.innerHTML += "<p><strong>You:</strong> " + message + "</p>";
 	conversation.innerHTML += "<p><strong>Eliza:</strong> " + answer + "</p>";
 }
 
 function reply(message) {
-	var reply = "Can you tell me more about that?";	
+	var reply = "Can you tell me more about that?";
 	var words = message.split(' ');
 	// Iterate over all rules and find the first one that matches
-	for(var i = 0; i<rules.length; i++) {
+	for (var i = 0; i < rules.length; i++) {
 		var match = matchPattern(rules[i].pattern, words, 0, 0);
 		if (match) {
 			// Reconstruct a reply - if a token is a number, find the
 			// matched word from 'match', otherwise just append the word
 			var res = "";
-			for(var token of rules[i].reply) {
-				if (typeof(token) == "number") {
+			for (var token of rules[i].reply) {
+				if (typeof (token) == "number") {
 					res += match[token]
 				}
 				else res += token;
 			}
 			reply = res;
 		}
-	}	
+	}
 	return reply;
 }
