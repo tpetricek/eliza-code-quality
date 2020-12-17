c467a7145d563f566c31ccd2319bea7b9a576d5b
Caleb Pike <calebpike91@gmail.com>
2020-12-17 17:09:53 +0000

    changed code layout and added new patterns

--- bot.js
+++ bot.js
@@ -2,47 +2,60 @@
 //@edited by Caleb Scotson Pike
 //@last modified 2020.12.16
 
+scrollToBottom();
+
 // A list of rules. Each rule is defined by a pattern consisting of
 // words or '*' and a reply which contains words or indices (starting
 // from zero) that refer to matched '*' word lists in the pattern.
-var rules =
-	[
-		{
-			"pattern": ["*", "I", "am", "*"],
-			"reply": ["Can you explain what made you ", 1, "?"]
-		},
-		{
-			"pattern": ["*", "I", "remember", "*"],
-			"reply": ["What made you think about ", 1, "?"]
-		},
-		{
-			"pattern": ["*", "loves", "*"],
-			"reply": ["How does that make you feel about ", 0, "?"]
-		},
-		{
-			"pattern": ["*", "mother", "*"],
-			"reply": ["Tell me more about your family."]
-		},
-		{
-			"pattern": ["*", "father", "*"],
-			"reply": ["Tell me more about your family."]
-		},
-	]
+var rules = [
+	{
+		pattern: ['*', 'I', 'am', '*'],
+		reply: ['Can you explain what made you ', 1, '?'],
+	},
+	{
+		pattern: ['*', 'I', 'remember', '*'],
+		reply: ['What made you think about ', 1, '?'],
+	},
+	{
+		pattern: ['*', 'loves', '*'],
+		reply: ['How does that make you feel about ', 0, '?'],
+	},
+	{
+		pattern: ['*', 'hates', '*'],
+		reply: ['How does that make you feel about ', 0, '?'],
+	},
+	{
+		pattern: ['*', 'love', '*'],
+		reply: ['What makes you love ', 1, '?'],
+	},
+	{
+		pattern: ['*', 'hate', '*'],
+		reply: ['What makes you hate ', 1, '?'],
+	},
+	{
+		pattern: ['*', 'mother', '*'],
+		reply: ['Tell me more about your family.'],
+	},
+	{
+		pattern: ['*', 'father', '*'],
+		reply: ['Tell me more about your family.'],
+	},
+]
 
 // Some words need to be replaced when recomposing the reply so that
 // "I remember my dog" -> "What made you think about *your* dog?"
-var replacements =
-	[
-		["my", "your"],
-		["My", "your"],
-		["me", "you"],
-		["Me", "you"],
-		["I", "you"],
-	]
+var replacements = [
+	['my', 'your'],
+	['My', 'your'],
+	['me', 'you'],
+	['Me', 'you'],
+	['I', 'you'],
+	['you', 'me'],
+]
 
 // Checks whether given words match given pattern (patternIndex and wordIndex are
-// indices of current position in pattern and word that we are 
-// looking at). Returns 'null' if matching fails or an array of 
+// indices of current position in pattern and word that we are
+// looking at). Returns 'null' if matching fails or an array of
 // matched word(s) that correspond to all '*' in the pattern.
 function matchPattern(pattern, words, patternIndex, wordIndex) {
 	if (patternIndex == pattern.length && wordIndex == words.length) {
@@ -51,8 +64,8 @@ function matchPattern(pattern, words, patternIndex, wordIndex) {
 	if (patternIndex == pattern.length) {
 		return null;
 	}
-	if (pattern[patternIndex] == "*") {
-		// Try matching '*' with anything between zero or all remaining words		
+	if (pattern[patternIndex] == '*') {
+		// Try matching '*' with anything between zero or all remaining words
 		for (var l = 0; l <= words.length - wordIndex; l++) {
 			var res = matchPattern(pattern, words, patternIndex + 1, wordIndex + l);
 			if (res) {
@@ -83,17 +96,17 @@ function saySomething() {
 	var message = document.getElementById('message').value;
 	var answer = myReply(message);
 	var conversation = document.getElementById('conversation');
-	conversation.innerHTML += "<p><strong>You:</strong> " + message + "</p>";
-	conversation.innerHTML += "<p><strong>Eliza:</strong> " + answer + "</p>";
+	conversation.innerHTML += '<p><strong>You:</strong> ' + message + '</p>';
+	conversation.innerHTML += '<p><strong>Eliza:</strong> ' + answer + '</p>';
 	scrollToBottom();
-	document.getElementById("message").value = '';
+	document.getElementById('message').value = '';
 }
 
 function myReply(message) {
-	var reply = "Can you tell me more about that?";
+	var reply = 'Can you tell me more about that?';
 	var words = message.split(' ');
 	// Iterate over all rules and find the first one that matches
-	if (message == "") {
+	if (message == '') {
 		reply = "You're being awfully quiet. Is something wrong?";
 	}
 	for (var i = 0; i < rules.length; i++) {
@@ -101,12 +114,11 @@ function myReply(message) {
 		if (match) {
 			// Reconstruct a reply - if a token is a number, find the
 			// matched word from 'match', otherwise just append the word
-			var res = "";
+			var res = ''
 			for (var token of rules[i].reply) {
-				if (typeof (token) == "number") {
-					res += match[token]
-				}
-				else res += token;
+				if (typeof token == 'number') {
+					res += match[token];
+				} else res += token;
 			}
 			reply = res;
 		}
@@ -119,10 +131,10 @@ function scrollToBottom() {
 	chatWindow.scrollTop = chatWindow.scrollHeight;
 }
 
-var enterClick = document.getElementById("message");
-enterClick.addEventListener("keyup", function (event) {
+var enterClick = document.getElementById('message')
+enterClick.addEventListener('keyup', function (event) {
 	if (event.code === 'Enter') {
 		event.preventDefault();
-		document.getElementById("sayit").click();
+		document.getElementById('sayit').click();
 	}
-});
+})
