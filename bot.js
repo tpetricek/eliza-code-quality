a487a56edc51d0bc924f010be5f63196746d0ce8
Alex Price <pricey1712@gmail.com>
2021-03-23 13:37:55 +0000

    Extract function refactoring for saySomething()

--- bot.js
+++ bot.js
@@ -53,10 +53,10 @@ function matchPattern(p, w, pi, wi) {
     }
     return null;
   }
-  if (wi == w.length)
+  if (wi == w.length) {
     return null;
-  if (p[pi] == w[wi]) 
-  {
+  }
+  if (p[pi] == w[wi]) {
       return matchPattern(p, w, pi+1, wi+1)
   }
 }
@@ -64,23 +64,28 @@ function matchPattern(p, w, pi, wi) {
 function saySomething() {
   var message = document.getElementById('message').value;
   var conversation = document.getElementById('conversation');
-  var answer = "Can you tell me more about that?";
-  var words = message.split(' ');
+  var answer = messageAnswer(message);
   
-  // Iterate over all rules and find the first one that matches
-    for (var i = 0; i<rules.length; i++) {
-      var match = matchPattern(rules[i].pattern, words, 0, 0)
-      if (match) {
-        // Reconstruct a reply - if a token is a number, find the
-        // matched word from 'match', otherwise just append the word
-        var res = ""
-        for (var token of rules[i].reply) {
-          if (typeof(token) == "number") res += match[token]
-          else res += token;
-        }
-      answer = res;
-      }
-    }
   conversation.innerHTML += "<p><strong>You:</strong> " + message + "</p>";
   conversation.innerHTML += "<p><strong>Eliza:</strong> " + answer + "</p>";
+}
+
+function messageAnswer(message) {
+  var words = message.split(' ');
+
+  // Iterate over all rules and find the first one that matches
+  for (var i = 0; i<rules.length; i++) {
+    var match = matchPattern(rules[i].pattern, words, 0, 0)
+    if (match) {
+      // Reconstruct a reply - if a token is a number, find the
+      // matched word from 'match', otherwise just append the word
+      var res = ""
+      for (var token of rules[i].reply) {
+        if (typeof(token) == "number") res += match[token]
+        else res += token;
+      }
+    return res;
+    }
+  }
+  return "Say something else!"
 }
